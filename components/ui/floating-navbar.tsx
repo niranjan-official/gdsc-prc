98b8545f3cce8605817d1c42e854bd2d54c5cd1b
niranjan-official <niranjansabarinath1521@gmail.com>
2024-06-30 16:27:16 +0530

    update: navbar multiple page support

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -8,55 +8,62 @@ import {
 } from "framer-motion";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
-import { IconCalendarMonth, IconHome, IconMessage, IconSitemap, IconUsers, IconUsersGroup } from "@tabler/icons-react";
+import {
+  IconCalendarMonth,
+  IconHome,
+  IconMessage,
+  IconSitemap,
+  IconUsers,
+  IconUsersGroup,
+} from "@tabler/icons-react";
+import { usePathname } from "next/navigation";
 
 const navItems = [
-    {
-      name: "Home",
-      link: "#home",
-      icon: <IconHome className="h-5 w-5 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "About",
-      link: "#about",
-      icon: <IconSitemap className="h-5 w-5 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Events",
-      link: "#events",
-      icon: <IconCalendarMonth className="h-5 w-5 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Team",
-      link: "#team",
-      icon: <IconUsersGroup className="h-5 w-5 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "FAQ",
-      link: "#faq",
-      icon: (
-        <IconMessage className="h-5 w-5 text-neutral-500 dark:text-white" />
-      ),
-    },
-  ];
+  {
+    name: "Home",
+    link: "#home",
+    icon: <IconHome className="h-5 w-5 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "#about",
+    icon: <IconSitemap className="h-5 w-5 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Events",
+    link: "#events",
+    icon: (
+      <IconCalendarMonth className="h-5 w-5 text-neutral-500 dark:text-white" />
+    ),
+  },
+  {
+    name: "Team",
+    link: "#team",
+    icon: (
+      <IconUsersGroup className="h-5 w-5 text-neutral-500 dark:text-white" />
+    ),
+  },
+  {
+    name: "FAQ",
+    link: "#faq",
+    icon: <IconMessage className="h-5 w-5 text-neutral-500 dark:text-white" />,
+  },
+];
 
-export const FloatingNav = ({
-  className,
-}: {
-  className?: string;
-}) => {
+export const FloatingNav = ({ className }: { className?: string }) => {
   const { scrollYProgress } = useScroll();
 
   const [visible, setVisible] = useState(true);
+  const pathname = usePathname();
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     // Check if current is not undefined and is a number
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
-      if(scrollYProgress.get() === 0 || null){
+      if (scrollYProgress.get() === 0 || null) {
         setVisible(true);
-      }else if(scrollYProgress.get() < 0.05) {
+      } else if (scrollYProgress.get() < 0.05) {
         setVisible(false);
       } else {
         if (direction < 0) {
@@ -87,18 +94,24 @@ export const FloatingNav = ({
           className
         )}
       >
-        {navItems.map((navItem: any, idx: number) => (
-          <Link
-            key={`link=${idx}`}
-            href={navItem.link}
-            className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
-            )}
-          >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
-          </Link>
-        ))}
+        {navItems.map((navItem: any, idx: number) => {
+          let navLink = navItem.link;
+          if (pathname.includes("team")) {
+            navLink = `/${navItem.link}`;
+          }
+          return (
+            <Link
+              key={`link=${idx}`}
+              href={navLink}
+              className={cn(
+                "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              )}
+            >
+              <span className="block sm:hidden">{navItem.icon}</span>
+              <span className="hidden sm:block text-sm">{navItem.name}</span>
+            </Link>
+          );
+        })}
       </motion.div>
     </AnimatePresence>
   );
