8ea828eba905de4bef3a1221a687fafe87c9d830
niranjan-official <niranjansabarinath1521@gmail.com>
2024-07-20 14:13:33 +0530

    update: 2022 members added

--- app/team/2022/page.tsx
+++ app/team/2022/page.tsx
@@ -6,23 +6,16 @@ import FadeUp from "@/Animations/FadeUp";
 const page = () => {
   return (
     <div className="w-full min-h-screen flex flex-col items-center bg-black px-10 md:px-16 lg:px-24 pt-32 pb-10">
-      <FadeUp>
-        <h3 className="text-4xl sm:text-6xl font-extrabold z-20">
-          Team Members
-        </h3>
-      </FadeUp>
-      <FadeUp>
-        <span className="text-4xl font-extralight mt-2 z-20">2022-2023</span>
-      </FadeUp>
+      <h3 className="text-4xl sm:text-6xl font-extrabold z-20">Team Members</h3>
+      <span className="text-4xl font-extralight mt-2 z-20">2022-2023</span>
       <div className="w-full flex justify-center flex-wrap gap-12 max-sm:gap-8 mt-8 z-20 px-3">
         {Team_2022.map((member) => (
-          <FadeUp key={member.id} >
             <TeamMemberCard
+              key={member.id}
               name={member.name}
               designation={member.designation}
               image={member.image}
             />
-          </FadeUp>
         ))}
       </div>
     </div>
